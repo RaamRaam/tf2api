3e84dd600204751f48c559b8452dbf899c416e19
Ramanathan Rm <rm.ramanathan@gmail.com>
2019-11-08 22:29:22 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ class ConvBN(tf.keras.Model):
 
 
 
-def linear_lr(global_step,data_len,batch_size,epochs,mode,peak_lr,repeat,interpolate,global_step):
+def linear_lr(global_step,data_len,batch_size,epochs,mode,peak_lr,repeat,interpolate):
   x=list(range(0,epochs+1,round(epochs*(1/repeat))))
   x= x + [epochs] if x[-1]!=epochs else x
   
@@ -67,7 +67,7 @@ def linear_lr(global_step,data_len,batch_size,epochs,mode,peak_lr,repeat,interpo
   return lr_func
 
 @timer
-def deep_learn(model, opt, loss, train, test):
+def deep_learn(model, opt, loss, train, test,global_step):
   train_loss = test_loss = train_correct = test_correct  = 0.0
   tf.keras.backend.set_learning_phase(1)
   
