f39693fd1ed26b999be21a615ce307d35b80e970
Ramanathan Rm <rm.ramanathan@gmail.com>
2019-11-08 22:12:03 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from .libraries import *
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, GlobalMaxPool2D, BatchNormalization
+from tqdm import tqdm_notebook as tqdm
 tf.keras.backend.set_floatx('float16')
 
 
