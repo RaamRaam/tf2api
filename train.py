86a782c8608ced38b7d33fafaf96d79cf4a37560
Ramanathan Rm <rm.ramanathan@gmail.com>
2019-11-17 02:23:42 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ class train(object):
     self.lr_modes=['constant','stepup','stepdown','angledup','angleddown']
     self.lr_mode=hparams['LR_MODE']
     # self.lr=self.linear_lr(self.train_ds.length,self.batch_size,self.epochs,self.lr_mode,self.lr_peak,self.lr_repeat,self.lr_interpolate)
-    self.lr=self.linear_lr(self)
+    self.lr=self.linear_lr()
 
     self.optimizer=hparams['OPTIMIZER'](self.lr)
     self.lossfunction=hparams['LOSSFUNCTION']
@@ -170,7 +170,7 @@ class train(object):
     else:
       x = list(range(0,self.epochs+1,round(self.epochs*(1/self.lr_repeat))))
       x = x + [self.epochs] if x[-1]!=self.epochs else x
-      
+
     
     if self.lr_mode=='stepup':
       z=[i+1 for i in x]
